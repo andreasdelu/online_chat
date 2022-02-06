69029e48bf97e7441b125d8cb76ce53d379b4fc5
andreasdelu <Andreasdelu@gmail.com>
2022-02-06 23:23:43 +0100

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,137 @@
+import { sanitizePackageName } from "snowpack/lib/cjs/util";
+import { io} from "socket.io-client"
+
+const joinRoomButton = document.getElementById("room-button");
+const leaveRoomButton = document.getElementById("leave-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+const allList = document.getElementById("all-list");
+const roomList = document.getElementById("room-list");
+
+
+const socket = io("http://localhost:3000")
+
+const storage = window.sessionStorage;
+
+
+socket.on("connect", () => {
+    displayMessage(`Connected!`);
+    setName();
+    
+    
+})
+
+function setName() {
+    let nickname;
+    if (storage.getItem("nickname") == null) {
+        nickname = prompt("Username: ");
+        if (nickname == null || nickname == '') {
+            const nickname = socket.id.substring(0,6);
+            socket.emit("set-name", nickname);
+        }
+        else if (nickname == storage.getItem("nickname")) {
+            socket.emit("set-name", nickname);
+            displayMessage("Your nickname is already: " + nickname);
+        }
+        
+        else {
+            socket.emit("set-name", (nickname));
+            displayMessage("Your nickname is now: " + nickname);
+            storage.setItem("nickname", nickname);
+        }
+    }
+    else {
+        nickname = storage.getItem("nickname");
+        socket.emit("set-name", (nickname));
+        displayMessage("Your nickname is now: " + nickname);
+    }
+}
+
+socket.on("recieve-message", (message) => {
+    displayMessage(message);
+})
+
+socket.on("populate-all", online => {
+    populateAll(online);
+})
+socket.on("populate-room", online => {
+    populateRoom(online);
+})
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    const room = roomInput.value;
+
+    if (message === "") return
+    yourMessage(message)
+    socket.emit("send-message", message, room)
+    messageInput.value = ""
+})
+
+joinRoomButton.addEventListener("click", () => {
+    const room = roomInput.value;
+    socket.emit("join-room", room, message => {
+        displayMessage(message);
+    })
+})
+leaveRoomButton.addEventListener("click", () => {
+    const room = roomInput.value;
+    roomList.innerHTML = '';
+    roomInput.value = '';
+    socket.emit("leave-room", room, message => {
+        displayMessage(message);
+    })
+})
+nameButton.addEventListener("click", () => {
+    if (nameInput.value == storage.getItem("nickname")) {
+        const nickname = nameInput.value;
+        displayMessage("Your nickname is already: " + nickname);
+    }
+    else {
+        const nickname = nameInput.value;
+        socket.emit("set-name", (nickname));
+        displayMessage("Your nickname is now: " + nickname);
+        storage.setItem("nickname", nickname);
+    }
+    if (nameInput.value == '') {
+        const nickname = socket.id.substring(0,6);
+        socket.emit("set-name", nickname);
+        nameInput.value = nickname;
+    }
+})
+
+function displayMessage(message) {
+    const div = document.createElement("div")
+    div.textContent = message
+    div.classList.add("message")
+    document.getElementById("message-container").append(div)
+}
+
+function yourMessage(message) {
+    const div = document.createElement("div")
+    div.textContent = "You: " + message
+    div.classList.add("you")
+    document.getElementById("message-container").append(div)
+}
+
+function populateAll(list) {
+    allList.innerHTML = '';
+    list = list.sort();
+    for (const user of list) {
+        const li = document.createElement("li")
+        li.textContent = user
+        allList.append(li)
+    }
+}
+function populateRoom(list) {
+    roomList.innerHTML = '';
+    list = list.sort();
+    for (const user of list) {
+        const li = document.createElement("li")
+        li.textContent = user
+        roomList.append(li)
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,57 @@
+
+const io = require('socket.io')(3000, {
+    cors:{
+        origin:["http://localhost:8080"]
+    }
+});
+
+io.on("connection", socket => {
+    socket.on("send-message", (message, room) => {
+        if (room === '') {
+            socket.broadcast.emit("recieve-message", socket.username + ": " + message)
+        }
+        socket.to(room).emit("recieve-message", socket.username + ": " + message)
+    })
+    socket.on("join-room", (room, cb) => {
+        socket.join(room)
+        getSocketsRoom(room);
+        cb(`You joined the room: ${room}`)
+        socket.to(room).emit("recieve-message", socket.username + ": " + "Joined the room!")
+    })
+    socket.on("leave-room", (room, cb) => {
+        socket.leave(room)
+        getSocketsRoom(room);
+        cb(`You left the room: ${room}`)
+        socket.to(room).emit("recieve-message", socket.username + ": " + "Left the room!")
+    })
+    socket.on("set-name", (nickname) => {
+        socket.username = nickname
+        getSockets()
+        
+    })
+    socket.on("disconnect", (reason) => {
+        console.log("User disconnected: " + reason);
+        getSockets();
+    })
+})
+
+async function getSockets() {
+    const sockets = await io.fetchSockets()
+    let usernames = [];
+    for (const socket of sockets) {
+        usernames.push(socket.username)
+
+        io.emit("populate-all", usernames)
+    }
+}
+
+async function getSocketsRoom(room){
+    const sockets = await io.in(room).fetchSockets()
+    let usernames = [];
+    for (const socket of sockets) {
+        usernames.push(socket.username)
+
+        io.in(room).emit("populate-room", usernames)
+    }
+}
+
